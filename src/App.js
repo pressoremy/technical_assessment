6eda4a7f42ef37199bad98f03551bfad39f9da8e
WDaven <daven.wesley@gmail.com>
2022-12-07 11:03:19 -0500

    add three pages with react-router
    Add Dashobard, Login, and Welcome pages, add imports react-router-dom and react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import {Route, Routes} from "react-router-dom";
+import { useEffect, useState} from "react";
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path ="/" element ={<Login />}/>
+      <Route path ="/welcome" element ={<Welcome/>}/>
+      <Route path ="/dashboard" element ={<Dashboard/>}/>
+    </Routes>
   );
 }
 
